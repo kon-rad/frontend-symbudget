040767fadfdbac86f28f31ba1167877ba13148d7
konradgnat <konradmgnat@gmail.com>
2017-11-18 14:24:34 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to SymBydget</h1>
+        </header>
+        <p className="App-intro">
+          This is the most useful and cleanest budget application ever!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
